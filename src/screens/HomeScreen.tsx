8c71bdf5b74191cad3dbb1151db49a4ca67f36b2
EcheCostabel <exequielcostabel@gmail.com>
2023-05-18 15:42:26 -0300

    first call to api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,15 +2,16 @@ import React, { useEffect } from 'react'
 import { Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import movieDB from '../api/movieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
     const navigation = useNavigation();
 
 
     useEffect(() => {
-      movieDB.get('/now_playing')
+      movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then( resp => {
-        console.log(resp.data)
+        console.log(resp.data.results[0].title)
       })
     }, [])
 
@@ -19,10 +20,7 @@ export const HomeScreen = () => {
   return (
     <View>
         <Text>HomeScreen</Text>
-        {/* <Button
-            title='ir detalle'
-            onPress={() => navigation.navigate('DetailScreen')}
-        /> */}
+
     </View>
   )
 }
