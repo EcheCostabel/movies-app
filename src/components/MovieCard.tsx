c4692e18b4bcff1a13bbb742e490911581166f88
EcheCostabel <exequielcostabel@gmail.com>
2023-05-22 20:27:41 -0300

    navegar a la pantalla de detalles

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
+import { useNavigation } from '@react-navigation/native';
 
 
 interface Props {
@@ -11,15 +12,21 @@ interface Props {
 
 export const MovieCard = ({movie, height = 420, width = 300} :Props) => {
 
-    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+    const navigation = useNavigation();
 
 
   return (
-    <View style={{
+    <TouchableOpacity 
+    onPress={() => navigation.navigate('DetailScreen', movie)}
+    activeOpacity={0.8}
+    style={{
         width,
         height,
         marginHorizontal: 5
     }}>
+        
         <View style= {styles.imgContainer}>
             <Image
                 source={{ uri }}
@@ -27,7 +34,7 @@ export const MovieCard = ({movie, height = 420, width = 300} :Props) => {
             />
             
         </View>
-    </View>
+    </TouchableOpacity>
   )
 };
 
