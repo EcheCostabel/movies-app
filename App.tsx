80dfea582122fc66d30c43f9daf587c2a0ea5c31
EcheCostabel <exequielcostabel@gmail.com>
2023-05-24 15:42:32 -0300

    continue with project

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react'
 import { Navigation } from './src/navigation/Navigation';
 
+
+
 const App = () => {
 
 
---
+++ metro.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    resolver: {
+      blacklistRE: /.*\/node_modules\/react\/dist\/deprecated\.js$/,
+    },
+  };
+  
